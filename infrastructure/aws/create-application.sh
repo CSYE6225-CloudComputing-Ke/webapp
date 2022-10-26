f568ffe5530524ce311582016e0e1788a7f95d16
Ke <wang.ke5@northeastern.edu>
2022-10-26 02:54:23 -0400

    test confi

---
+++ infrastructure/aws/create-application.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+stackname=$1
+ami=$2
+instanceName="$stackname-csye6225-instance"
+
+vpcId=$(aws ec2 describe-vpcs --filters "Name=cidr-block-association.cidr-block,Values=10.0.0.0/16" --query "Vpcs[0].VpcId" --output text)
+echo $vpcId
+
+
+
+
+aws cloudformation create-stack --template-body file://./csye6225-infra.json --stack-name ${stackname} --capabilities "CAPABILITY_NAMED_IAM" --parameters ParameterKey=AMI,ParameterValue="$ami"
+
+echo "Creating! Please wait until done"
+
+aws cloudformation wait stack-create-complete --stack-name ${stackname}
+
+echo "Done"
---
+++ infrastructure/aws/create-stack.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+stackname=$1
+
+VpcName="$stackname-csye6225-vpc"
+GatewayName="$stackname-csye6225-InternetGateway"
+RouteTableName="$stackname-csye6225-rt"
+
+aws cloudformation create-stack --stack-name $stackname --template-body file://./csye6225-cf-networking.json --parameters ParameterKey=vpcName,ParameterValue=$VpcName ParameterKey=gatewayName,ParameterValue=$GatewayName ParameterKey=routeTableName,ParameterValue=$RouteTableName
+
+echo "Creating! Please wait until done"
+
+aws cloudformation wait stack-create-complete --stack-name $stackname
+
+echo "Done"
