a31e3311eeb1e58c005c2caaffd0812081e414bc
Ke <wang.ke5@northeastern.edu>
2022-10-30 20:52:05 -0400

    assign5

--- infrastructure/codedeploy/test.sh
+++ infrastructure/codedeploy/test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo service tomcat stop
-kill -9 $(lsof -i:8080 | awk '{print $2}' | tail -n 2)
-cd /home/centos
+kill -9 "$(lsof -i:8080 | awk '{print $2}' | tail -n 2)"
+cd /home/centos || exit
 sudo chmod 777 -R CloudWebApp/target/classes
